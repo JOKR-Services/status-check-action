88e6a5b951e36be435fadba4d12257c1aae98d4f
Krisztian Sala <salakrisztian@gmail.com>
2022-11-11 15:37:43 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # status-check-action
-Github Action for checking if all the jobs passed that ran in a workflow
+GitHub Action for checking if all the jobs passed that ran in a workflow
+
+## Usage 
+
+Add it to the end of your workflow in a separate job. It needs to depend on all previous jobs, so it can check the status of each.
+### Example
+```yaml
+jobs:
+  ...
+  status-check:
+    runs-on: ubuntu-latest
+    name: Status Check
+    if: always()
+    needs: ['job-1', 'job-2', 'job-n']
+    steps:
+      - uses: JOKR-Services/status-check-action@main
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
+
+## Development
+
+Add your changes to the `index.ts` file. Once done, run the `npm run build` command to create the javascript distributable.
+When finished, create a pull request.
---
+++ index.ts
@@ -0,0 +1,45 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { Octokit } from "@octokit/action";
+
+const octokit = new Octokit();
+interface ListJobsConfig {
+    [key: string]: string | number;
+    owner: string;
+    repo: string;
+    run_id: number;
+}
+
+async function listJobs(config: ListJobsConfig): Promise<any[]> {
+    try {
+        const jobList = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs', config);
+        return jobList.data.jobs;
+    } catch (error: any) {
+        core.error('⚠️ Error getting job list ');
+        core.error(error);
+        return [];
+    }
+}
+
+async function main() {
+    const { runId, runNumber } = github.context;
+    const repoFullName = github.context.payload.repository?.full_name?.split('/');
+    if(repoFullName == undefined) return core.setFailed('Repository name could not be determined');
+
+    const config: ListJobsConfig = {
+        owner: repoFullName[0],
+        repo: repoFullName[1],
+        run_id: runId,
+        attempt_number: runNumber
+    }
+    
+    const jobList = await listJobs(config);
+    for (const job of jobList) {
+        if(job.conclusion == 'failure'){
+            return core.setFailed(`❌ All jobs need to pass correctly.`);
+        }
+    }
+    core.info("✅ All jobs passed");
+}
+
+main();
